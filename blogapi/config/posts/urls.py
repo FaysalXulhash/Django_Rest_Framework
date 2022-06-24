cd47ebab80d48c580cce11e8aef517cb59c1568b
FaysalXulhash <faysalahammed80@gmail.com>
2022-06-24 23:47:42 +0600

    Schema and Documention

--- blogapi/config/posts/urls.py
+++ blogapi/config/posts/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
-from .views import PostList, PostDetail
+from .views import UserViewSet, PostViewSet
+from rest_framework.routers import SimpleRouter
 
 
-urlpatterns = [
-    path('<int:pk>/', PostDetail.as_view()),
-    path('', PostList.as_view()),
-
-]
+router = SimpleRouter()
+router.register('users', UserViewSet, basename='users')
+router.register('', PostViewSet, basename='posts')
+urlpatterns = router.urls
